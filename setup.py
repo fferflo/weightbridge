63d0c0a887ccf145f4d0d8011d7915294d1d7e85
Florian Fervers <florian.fervers@gmail.com>
2023-12-18 15:01:16 +0100

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="weightbridge",
-    version="0.0.1",
+    version="0.0.2",
     python_requires=">=3",
     description="Library to map (deep learning) model weights between different model implementations.",
     author="Florian Fervers",
